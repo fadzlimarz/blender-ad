2b9f51cad5241f79553986ddbabab7f5a227b3c6
Fadzli Marzuki <fadzli@Fadzlis-MacBook-Air.local>
2023-12-15 17:11:11 -0500

    Fixed view for smaller screens

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Video from "next-video";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl pb-10 w-full items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex flex-col items-center justify-between p-24">
+      <div className="max-w-5xl pb-10 w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Blender Advertisement for DIG012AS2AE
         </p>
